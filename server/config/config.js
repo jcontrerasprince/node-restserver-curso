3f898ad292657a460479dde247185e0c0af326f2
jcontrerasprince <jcontrerasprince@gmail.com>
2020-09-20 10:53:59 -0500

    Google Sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -11,12 +11,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 //===================
-// Base de datos
-//===================
-let urlDB;
-
-
-//===================
 // Vencimiento del Token
 //===================
 // 60 segundos
@@ -32,6 +26,10 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 
 
+//===================
+// Base de datos
+//===================
+let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
@@ -39,3 +37,10 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
+
+
+//===================
+// Google Client ID
+//===================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '999231375452-behvujt2kmrom5ehjgdlsg8rb9oplqos.apps.googleusercontent.com';
